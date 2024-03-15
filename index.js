69f7586b80fbc8196f2a5b3e41bb22834f0ce9e1
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-15 17:37:37 -0300

    configuramos los middlewares cors, morgan y dotenv

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
-console.log('Hola Mundo')
+import cors from 'cors'
+import 'dotenv/config';
+import morgan from 'morgan';
 
 const app = express();
 app.set('port',process.env.PORT ||4002);
 app.listen(app.get('port'),()=>{
     console.log('Estoy en el puerto '+ app.get('port'))
 })
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))

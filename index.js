4ac8fbedddf7c0b8a63d5aee0a31e73db9cdadd2
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-15 16:48:27 -0300

    agregue el puerto 4002 para mi backend

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import express from 'express';
 console.log('Hola Mundo')
+
+const app = express();
+app.set('port',4002);
+app.listen(app.get('port'),()=>{
+    console.log('Estoy en el puerto '+ app.get('port'))
+})

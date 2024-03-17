afcc991b0af699bb4fe98a44908021cd02e062bd
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-17 17:04:20 -0300

    agregue la funcion para editar mi color

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -27,6 +27,20 @@ export const obtenerColor = async (req,res)=>{
    res.status(200).json(colorBuscado);
   }catch(error){
     console.log(error)
-    res.status(404).json({mensaje: 'No se encontro la tarea solicitada'})
+    res.status(404).json({mensaje: 'No se encontro el color solicitado'})
   }
  }
+
+ export const editarColor = async(req,res)=>{
+  try{
+   const buscarColor = await Color.findById(req.params.id);
+   if(!buscarColor){
+     return res.status(404).json({mensaje:'No se pudo editar el color, el id es incorrecto'})
+   }
+   await Color.findByIdAndUpdate(req.params.id, req.body);
+   res.status(200).json({mensaje:'El color fue modificado exitosamente'});
+  }catch(error){
+      console.error(error)
+      res.status(500).json({mensaje:'Ocurrio un error al intentar editar el color'})
+  }
+  }
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,8 +1,8 @@
 import { Router} from "express";
-import {crearColor, listarColores, obtenerColor} from "../controllers/colores.controllers.js"
+import {crearColor, editarColor, listarColores, obtenerColor} from "../controllers/colores.controllers.js"
 
 const router = Router();
 
 router.route('/colores').get(listarColores).post(crearColor)
-router.route('/color/:id').get(obtenerColor)
+router.route('/color/:id').get(obtenerColor).put(editarColor)
 export default router;

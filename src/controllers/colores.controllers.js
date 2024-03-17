1d93c57d84537c70717bc695267dcdc3ea6c0c65
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-17 16:50:14 -0300

    agregue la funcion para poder crear un color

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -4,3 +4,14 @@ export  const listarColores=(req,res)=>{
     console.log('desde listar colores');
     res.send('Enviar lista de colores...')
 }
+
+export const crearColor = async (req,res)=>{
+  try{
+    const colorNuevo= new Color(req.body);
+    await colorNuevo.save();
+    res.status(201).json({mensaje:'El color fue creado correctamente'})
+   }catch(error){
+  console.log(error)
+  res.status(400).json({mensaje: 'No se pudo procesar la solicitud de crear color'})
+  }
+}
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,8 +1,8 @@
 import { Router} from "express";
-import {listarColores} from "../controllers/colores.controllers.js"
+import {crearColor, listarColores} from "../controllers/colores.controllers.js"
 
 const router = Router();
 
-router.route('/colores').get(listarColores)
+router.route('/colores').get(listarColores).post(crearColor)
 
 export default router;

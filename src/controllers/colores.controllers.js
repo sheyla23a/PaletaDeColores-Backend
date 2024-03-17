4cc04e1289da92b9d8ebbf44aeda4622369ccbe7
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-17 16:58:21 -0300

    agregue la funcion para obtener la lista de colores y obtener un color por su ID

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -1,8 +1,13 @@
 import Color from "../database/model/color.js";
 
-export  const listarColores=(req,res)=>{
-    console.log('desde listar colores');
-    res.send('Enviar lista de colores...')
+export  const listarColores=async(req,res)=>{
+  try{
+    const colores = await Color.find();
+    res.status(200).json(colores);
+  }catch(error){
+   console.log(error)
+   res.status(404).json({mensaje: 'No se encontro la lista de colores'})
+  }
 }
 
 export const crearColor = async (req,res)=>{
@@ -15,3 +20,13 @@ export const crearColor = async (req,res)=>{
   res.status(400).json({mensaje: 'No se pudo procesar la solicitud de crear color'})
   }
 }
+
+export const obtenerColor = async (req,res)=>{
+  try{
+   const colorBuscado = await Color.findById(req.params.id);
+   res.status(200).json(colorBuscado);
+  }catch(error){
+    console.log(error)
+    res.status(404).json({mensaje: 'No se encontro la tarea solicitada'})
+  }
+ }
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,8 +1,8 @@
 import { Router} from "express";
-import {crearColor, listarColores} from "../controllers/colores.controllers.js"
+import {crearColor, listarColores, obtenerColor} from "../controllers/colores.controllers.js"
 
 const router = Router();
 
 router.route('/colores').get(listarColores).post(crearColor)
-
+router.route('/color/:id').get(obtenerColor)
 export default router;

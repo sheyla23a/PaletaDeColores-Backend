03129405cb7d2e10dde366368efae50622130082
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-21 17:15:51 -0300

    se realizaron los cambios necesarios para conectar mi back con mi front de paleta de colores

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -31,19 +31,6 @@ export const obtenerColor = async (req,res)=>{
   }
  }
 
- export const editarColor = async(req,res)=>{
-  try{
-   const buscarColor = await Color.findById(req.params.id);
-   if(!buscarColor){
-     return res.status(404).json({mensaje:'No se pudo editar el color, el id es incorrecto'})
-   }
-   await Color.findByIdAndUpdate(req.params.id, req.body);
-   res.status(200).json({mensaje:'El color fue modificado exitosamente'});
-  }catch(error){
-      console.error(error)
-      res.status(500).json({mensaje:'Ocurrio un error al intentar editar el color'})
-  }
-  }
 
   export const borrarColor = async(req,res)=>{
     try{
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -1,8 +1,8 @@
 import { Router} from "express";
-import {borrarColor, crearColor, editarColor, listarColores, obtenerColor} from "../controllers/colores.controllers.js"
+import {borrarColor, crearColor, listarColores, obtenerColor} from "../controllers/colores.controllers.js"
 
 const router = Router();
 
 router.route('/colores').get(listarColores).post(crearColor)
-router.route('/color/:id').get(obtenerColor).put(editarColor).delete(borrarColor)
+router.route('/color/:id').get(listarColores).delete(borrarColor)
 export default router;
